f134148b5eabe090631b7da13df9c36051292a79
Estebanfuentesinacap <esteban.fuentes18@inacapmail.cl>
2023-04-24 09:39:18 -0400

    Integracion node modules

--- arrays.js
+++ arrays.js
@@ -2,6 +2,23 @@ const abecedario =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ',
 
 const email = ['@' && '.com','.es','.cl','.net','.org','.edu','.gob']
 
+palabra = 'hola esta palabra es demasiado larga';
+
+function esPalindromo(palabra) {
+    if (palabra.length > 10) {
+      return "Error: palabra ingresada excede las 10 letras";
+    }
+    let invertida = palabra.split("").reverse().join("");
+    if (palabra === invertida) {
+      return "La palabra ingresada es un palíndromo";
+    } else {
+      return "La palabra ingresada no es un palíndromo";
+    }
+}
+
+console.log(esPalindromo)
+
+
 module.exports = {
     email,
     abecedario
